189063dd35615febc2ed9ef7802e7250569c9dbc
Gregory ELBAJOURY <34866703+Injechta@users.noreply.github.com>
2023-09-19 15:56:56 +0200

    Update resources.tf

--- terraform/resources.tf
+++ terraform/resources.tf
@@ -1,10 +1,9 @@
-# resources.tf
 # Création d'une application Azure AD pour le Service Principal
 resource "azuread_application" "example" {
-  display_name               = "GitHub-IAC" # Mettez à jour le nom de l'application ici
-  available_to_other_tenants = false
-  oauth2_allow_implicit_flow = true
-  reply_urls                 = [""] # Ajout de l'URL de redirection avec une valeur vide
+  display_name               = local.app_details.display_name
+  available_to_other_tenants = local.app_details.account_type
+  oauth2_allow_implicit_flow = local.app_details.oauth2_flow
+  reply_urls                 = [""]
 }
 
 # Création du Service Principal à partir de l'application Azure AD
@@ -15,6 +14,6 @@ resource "azuread_service_principal" "example" {
 # Création d'un secret associé au Service Principal
 resource "azuread_service_principal_password" "example" {
   service_principal_id = azuread_service_principal.example.id
-  description          = var.secret_description
-  end_date_relative    = "8760h"  # 365 days in hours
+  description          = local.secret_details.description
+  end_date_relative    = local.secret_details.duration
 }
