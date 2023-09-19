1fec60c7b0117786b7da515962da8fc0cb0b5657
Gregory ELBAJOURY <34866703+Injechta@users.noreply.github.com>
2023-09-19 15:55:05 +0200

    Create locals.tf

---
+++ terraform/locals.tf
@@ -0,0 +1,13 @@
+# locals.tf
+locals {
+  app_details = {
+    display_name = "GitHub-IAC"
+    account_type = false # Ceci assure que seuls les comptes de votre organisation peuvent accéder à cette application (équivalent à Single tenant)
+    oauth2_flow  = true
+  }
+
+  secret_details = {
+    description = "Client secret for automation"
+    duration    = "8760h"  # 365 days in hours
+  }
+}
